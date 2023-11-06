d72602e167192f7ffd65f07777d0a4f2e025febe
OumaymaCharrad1999 <charradoumayma1@gmail.com>
2023-11-06 12:40:53 +0100

    Test

--- script.groovy
+++ script.groovy
@@ -25,7 +25,7 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: "Docker_Hub_Credentials", passwordVariable: "PASS", usernameVariable: "USER")]) {
         sh "docker build -t oumaymacharrad/my-repo:pet-store-app-${IMAGE_VERSION} ."
         echo "Pushing the Docker Image to Docker Hub..."
-        sh "docker login -u $USER"
+        sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push oumaymacharrad/my-repo:pet-store-app-${IMAGE_VERSION}"
     }
 }
