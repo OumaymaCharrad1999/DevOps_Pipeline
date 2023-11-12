eeb7809da9d2b68562dc22977178dc3177f8e9b7
OumaymaCharrad1999 <charradoumayma1@gmail.com>
2023-11-12 16:24:41 +0100

    Configure Nexus Repository Manager

--- script.groovy
+++ script.groovy
@@ -17,7 +17,7 @@ def sonarScan() {
 
 def publishToNexus() {
     echo "Pushing the JAR file to Nexus..."
-    sh "mvn clean deploy -DskipTests -DrepositoryId=nexus-snapshots"
+    sh "mvn clean deploy -DskipTests"
 }
 
 def buildImage() {
